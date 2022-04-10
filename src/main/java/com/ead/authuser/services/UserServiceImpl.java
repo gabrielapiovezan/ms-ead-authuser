227f49baf3945e1e7e58ead8f27680abbcfe106f
gabipiovezan <gabrielapiovezan@live.com>
2022-04-10 19:22:10 -0300

    Criando filtros avançados

--- src/main/java/com/ead/authuser/services/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/UserServiceImpl.java
@@ -4,11 +4,9 @@ import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
-
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
