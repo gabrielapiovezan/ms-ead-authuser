079a968b5a331ae02e17c437e41a551a66dde2ac
gabipiovezan <gabrielapiovezan@live.com>
2022-04-27 22:33:51 -0300

    Criando padrao de data iso8601

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -53,10 +53,10 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     @Column
     private String imageUrl;
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'T'")
     private LocalDateTime lastUpdateDate;
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'T'")
     private LocalDateTime creationDate;
 
 }
