2b7fd9794a8e9badf64a6cdb230ac3365561f5b2
gabipiovezan <gabrielapiovezan@live.com>
2022-08-01 23:08:34 -0300

    Incluindo role no end-point e incluindo nova validacao

--- src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,11 +1,14 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDTO;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,14 +27,19 @@ public class InstructorController {
 
     private final UserService userService;
 
+    private final RoleService roleService;
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDTO instructorDTO) {
 
         var optUser = userService.findById(instructorDTO.getUserId());
-
+        var roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR)
+                .orElseThrow(() -> new RuntimeException("Error: Role not found"));
         return optUser.<ResponseEntity<Object>>map(userModel -> {
             userModel.setUserType(UserType.INSTRUCTOR);
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userModel.getRoles().add(roleModel);
             userService.updateUser(userModel);
             return ResponseEntity.status(OK).body(userModel);
         }).orElseGet(() -> ResponseEntity.status(NOT_FOUND).body("User not found"));
